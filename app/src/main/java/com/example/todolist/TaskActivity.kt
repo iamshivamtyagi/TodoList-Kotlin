d8f14f82fa49ef62e2b1a0a54517945dbe73857b
Shivam Tyagi <shivamtyagi837@gmail.com>
2020-05-08 19:20:26 +0530

    date picker implemented

--- app/src/main/java/com/example/todolist/TaskActivity.kt
+++ app/src/main/java/com/example/todolist/TaskActivity.kt
@@ -2,18 +2,21 @@ package com.example.todolist
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_task.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 const val DB_NAME = "todo.db"
 
-class TaskActivity : AppCompatActivity() {
+class TaskActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var myCalendar: Calendar
 
     lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
-
 
     val db by lazy {
         Room.databaseBuilder(
@@ -27,6 +30,49 @@ class TaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
 
-        
+        dateEdt.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.dateEdt -> {
+                setListener()
+            }
+        }
+    }
+
+    private fun setListener() {
+        myCalendar = Calendar.getInstance()
+
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                myCalendar.set(Calendar.YEAR, year)
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                myCalendar.set(Calendar.MONTH, month)
+
+                updateDate()
+            }
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            dateSetListener,
+            myCalendar.get(Calendar.YEAR),
+            myCalendar.get(Calendar.DAY_OF_MONTH),
+            myCalendar.get(Calendar.MONTH)
+        )
+
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis()
+        datePickerDialog.show()
+    }
+
+    private fun updateDate() {
+        // FRI, 5 May 2020
+        val myFormat = "EEE, d MMM yyyy"
+
+        val sdf = SimpleDateFormat(myFormat)
+        dateEdt.setText(sdf.format(myCalendar.time))
+
+        timeInptLay.visibility = View.VISIBLE
+    }
+
 }
